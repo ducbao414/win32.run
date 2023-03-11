0f6f23e300f57f5393e2aca38a2a8d90c6bfb965
Bao Nguyen <ducbao414@gmail.com>
2023-03-11 20:25:12 +0700

    change build folder

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,7 @@ import adapter from '@sveltejs/adapter-auto';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter({ out: 'build' })
+    adapter: adapter()
     
   },
 
