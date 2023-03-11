31f2bb17a32fdc03a64135548c8c4f27b6a84db4
Bao Nguyen <ducbao414@gmail.com>
2023-03-11 20:47:27 +0700

    revert back to adapter-node

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,10 @@
 import preprocess from "svelte-preprocess";
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter()
+    adapter: adapter({ out: 'build' })
     
   },
 
